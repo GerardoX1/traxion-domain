10deda7bda96ae3b4a678c2e0db6e56de2ba12cf
Luis <gerardo.fosado@cargamos.com>
2025-03-05 22:28:39 -0600

    updated

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
     name="traxion-domain",
@@ -8,18 +8,24 @@ setup(
     description="traxion Domain Python",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords="traxion, domain, library, python",
     license="MIT",
-    packages=find_packages(include=["traxion*"]),
-    package_data={"traxion_domain": ["*.json"]},
+    packages=find_packages(),
+    package_data={"": ["*.json"]},
+    namespace_packages=["traxion"],
     python_requires=">=3.11",
     install_requires=[
         "pydantic==2.5.3",
     ],
+    classifiers=["Programming Language :: Python :: 3"],
     extras_require={
         "dev": [
             "pytest==7.2.0",
             "pytest-mock==3.10.0",
         ]
     },
+    zip_safe=True,
+    test_suite="tests",
     url="https://github.com/GerardoX1/traxion-domain.git",
 )
