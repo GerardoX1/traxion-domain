28822200a69f850e2b9ff42dbb5b58af892203ec
Luis <gerardo.fosado@cargamos.com>
2025-03-06 12:50:39 -0600

    paginated query

--- traxion/services/v1/service_orders.py
+++ traxion/services/v1/service_orders.py
@@ -1,3 +1,5 @@
+from typing import Iterator, List, Optional, Tuple
+
 from traxion.models.v1.service_orders import ServiceOrderModel
 from traxion.services.base_services.base_service import BaseService
 
@@ -9,3 +11,15 @@ class VehicleService(BaseService[ServiceOrderModel]):
         self.__repository__ = repository
         self.__verbose__ = verbose
         super().__init__(*args, **kwargs)
+
+    def paginated_query(
+        self,
+        conditions: List[tuple] = None,
+        page: int = 1,
+        limit: int = 10,
+        sort: Optional[List[Tuple[str, int]]] = None,
+    ) -> Tuple[int, Iterator[ServiceOrderModel]]:
+        count, query_result = self._query_paginated(
+            page=page, limit=limit, and_conditions=conditions, sort=sort
+        )
+        return count, map(lambda x: ServiceOrderModel(**x), query_result)
